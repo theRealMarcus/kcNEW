6763c34126a7f2eb2544af4e8550202a6a19f072
Marcus Camden <mecamden@gmail.com>
2015-05-10 12:52:12 -0600

    Recommit

---
+++ js/myFunctions.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	//Lights img up when you hover over the img
+	$('#bttt').mouseover(function(){
+		$(this).fadeTo((0.25 * 1000), 1);
+	});
+	$('#bttt').mouseout(function(){
+		$(this).fadeTo((0.25 * 1000), 0.3)
+	});
+
+	//Scrolls the document up to the top
+
+	//Search overlay
+	$('.searchToggle').click(function(){
+		$('#searchOverlay').fadeToggle(300);
+	});
+
+	//Slide Toggle the SharePoint ribbon
+	
+});
